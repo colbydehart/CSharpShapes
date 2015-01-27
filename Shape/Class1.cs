96deb9f05d914da596b965b5059e60696e365ef8
Colby DeHart <colbydehart@gmail.com>
2015-01-26 19:17:02 -0600

    init commit

---
+++ Shape/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace CSharpShape
+{
+    /// <summary>
+    /// Shape superclass for all shapes.
+    /// </summary>
+    public class Shape
+    {
+        
+    }
+}
