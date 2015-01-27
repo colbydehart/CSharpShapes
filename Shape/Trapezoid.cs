c656efbcf4d5a41d3690534fdfde183438e48a64
Colby DeHart <colbydehart@gmail.com>
2015-01-27 13:51:24 -0600

    Halfway through trapezoids

---
+++ Shape/Trapezoid.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpShape
+{
+    public class Trapezoid : Shape
+    {
+        private decimal top;
+        public decimal Top 
+        { 
+            get{ return top; } 
+            set{ this.top = value; }
+        }
+        private decimal bottom;
+
+        public decimal Bottom 
+        { 
+            get{ return bottom; } 
+            set{ this.bottom = value; }
+        }
+
+        private decimal height;
+        public decimal Height 
+        { 
+            get{ return height; } 
+            set{ this.height = value; }
+        }
+
+        public decimal AcuteAngle { get; set; }
+        public decimal ObtuseAngle { get; set; }
+
+        public Trapezoid(decimal top, decimal bottom, decimal height)
+        {
+            if (top == bottom)
+                throw new ArgumentException("Trapezoid cannot be a rectangle");
+            if (top <= 0 || bottom <= 0 || height <= 0)
+                throw new ArgumentException("dimensions must be positive");
+            this.top = top;
+            this.bottom = bottom;
+            this.height = height;
+            decimal wing = Math.Abs(bottom - top)/2;
+            this.AcuteAngle = (decimal)(Math.Atan((double)(height / wing))*(180/Math.PI));
+            this.ObtuseAngle = 180 - this.AcuteAngle;
+        }
+
+        public override decimal Area()
+        {
+            return (top + bottom) / 2 * height;
+        }
+
+        public override decimal Perimeter()
+        {
+            decimal wing = Math.Abs(top-bottom)/2;
+            double angledSide = Math.Sqrt((double)(wing * wing + height*height));
+            return (decimal)(angledSide * 2) + bottom + top;
+        }
+
+        public override int SidesCount
+        {
+            get { return 4; }
+        }
+
+        public override void Scale(int percent)
+        {
+            if (percent <= 0)
+                throw new ArgumentException("percentage must be a positive integer");
+            this.top = this.top * percent / 100;
+            this.bottom = this.bottom * percent / 100;
+            this.height = this.height * percent / 100;
+        }
+    }
+}
---
+++ UnitTestShape/SquareTest.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharpShape;
+
+namespace UnitTestShape
+{
+    [TestClass]
+    public class SquareTest
+    {
+        [TestMethod]
+        public void SquareIsConstructedWithSingleArgument()
+        {
+            Square square = new Square(3);
+            Assert.AreEqual(square.Width, 3);
+            Assert.AreEqual(square.Height, 3);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SquareSanityChecksWidth()
+        {
+            Square square = new Square(0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SquareSanityChecksWidthPositivity()
+        {
+            Square square = new Square(-1);
+        }
+
+        [TestMethod]
+        public void SquareScale200()
+        {
+            Square squ = new Square(10);
+            squ.Scale(200);
+            Assert.AreEqual(squ.Width, 20);
+            Assert.AreEqual(squ.Height, 20);
+        }
+
+        [TestMethod]
+        public void SquareScale150()
+        {
+            Square squ = new Square(10);
+            squ.Scale(150);
+            Assert.AreEqual(squ.Width, 15);
+            Assert.AreEqual(squ.Height, 15);
+        }
+
+        [TestMethod]
+        public void SquareScale100()
+        {
+            Square squ = new Square(10);
+            squ.Scale(100);
+            Assert.AreEqual(squ.Height, 10);
+            Assert.AreEqual(squ.Width, 10);
+        }
+
+        [TestMethod]
+        public void SquareScale37()
+        {
+            Square squ = new Square(10);
+            squ.Scale(37);
+            Assert.AreEqual(squ.Height, (decimal)3.7);
+            Assert.AreEqual(squ.Width, (decimal)3.7);
+        }
+
+        [TestMethod]
+        public void SquareScaleUpAndDown()
+        {
+            Square squ = new Square(10);
+            squ.Scale(50);
+            squ.Scale(200);
+            Assert.AreEqual(squ.Width, 10);
+            Assert.AreEqual(squ.Height, 10);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SquareScale0()
+        {
+            Square squ = new Square(10);
+            squ.Scale(0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SquareScaleNegative()
+        {
+            Square squ = new Square(10);
+            squ.Scale(-100);
+        }
+
+        [TestMethod]
+        public void SquareSidesCount()
+        {
+            Square squ = new Square(10);
+            Assert.AreEqual(squ.SidesCount, 4);
+        }
+
+        [TestMethod]
+        public void SquareArea()
+        {
+            Square squ = new Square(10);
+            Assert.AreEqual(squ.Area(), 100);
+        }
+
+        [TestMethod]
+        public void SquareLargerArea()
+        {
+            Square squ = new Square(20);
+            Assert.AreEqual(squ.Area(), 400);
+        }
+
+        [TestMethod]
+        public void SquarePerimeter()
+        {
+            Square squ = new Square(10);
+            Assert.AreEqual(squ.Perimeter(), 40);
+        }
+
+        [TestMethod]
+        public void SquareLargerPerimeter()
+        {
+            Square squ = new Square(20);
+            Assert.AreEqual(squ.Perimeter(), 80);
+        }
+
+        [TestMethod]
+        public void SquareDefaultColors()
+        {
+            Rectangle shape = new Rectangle(10, 15);
+            Assert.AreEqual(System.Drawing.Color.Bisque, shape.FillColor);
+            Assert.AreEqual(System.Drawing.Color.Tomato, shape.BorderColor);
+        }
+
+
+    }
+}
